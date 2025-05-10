fd2068e8dfee7968fa691b840f9e423d51d589cd
moussaguillaume <moussaguillaume@hotmail.fr>
2025-05-10 11:26:02 +0200

    modification et mise en sécuriter la connection a la basse de donné

--- db.php
+++ db.php
@@ -1,11 +1,20 @@
 <?php
-$host = 'localhost';
-$user = 'root'; // à adapter selon ton serveur
-$password = ''; // à adapter aussi
-$dbname = 'ESPORTIFY'; // nom de ta base
+// Inclure Composer autoload pour charger les dépendances, y compris phpdotenv
+require_once __DIR__ . '../../ESPORTIFY/vendor/autoload.php';
 
-// Créer une connexion
-$conn = new mysqli($servername, $username, $password, $dbname);
+// Charger le fichier .env
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// Récupérer les variables d'environnement
+$host = $_ENV['DB_HOST'];
+$port = $_ENV['DB_PORT'];
+$user = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
+$dbname = $_ENV['DB_NAME'];
+
+// Créer une connexion à la base de données avec mysqli
+$conn = new mysqli($host, $user, $password, $dbname, $port);
 
 // Vérifier la connexion
 if ($conn->connect_error) {
@@ -14,5 +23,9 @@ if ($conn->connect_error) {
     echo "Connexion réussie à la base de données !";
 }
 
-// Pour forcer l’UTF-8
+// Forcer l’utilisation de l’UTF-8 pour la connexion
 mysqli_set_charset($conn, "utf8");
+
+// N'oubliez pas de fermer la connexion à la base de données si vous n'en avez plus besoin
+ $conn->close();
+
